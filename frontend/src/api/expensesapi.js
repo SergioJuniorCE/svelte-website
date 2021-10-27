58a6602a6230c92230160734e0ad4ec9ac42f668
Sergio Junior Castro Egurrolar <sergiojuniorce@gmail.com>
2021-10-26 20:27:09 -0700

    asd

--- frontend/src/api/expensesapi.js
+++ frontend/src/api/expensesapi.js
@@ -1,5 +1,6 @@
 import { variables } from "$lib/variables";
 
+// Gets expenses from the api
 export const getExpenses = async () => {
   const res = await fetch(`${variables.API_URL}/expenses/`)
   const data = await res.json();
