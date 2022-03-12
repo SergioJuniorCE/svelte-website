a61989fd661e484b6928ee2cb4774115032d2876
Sergio Junior Castro Egurrolar <sergiojuniorce@gmail.com>
2022-03-12 14:39:32 -0700

    Create index.svelte

---
+++ src/routes/meals/index.svelte
@@ -0,0 +1,117 @@
+<script lang="ts">
+	const meals = {
+		breakfast: [
+			{
+				name: 'Bagel',
+				calories: 300,
+				fat: 10,
+				carbs: 20,
+				protein: 5
+			},
+			{
+				name: 'Eggs',
+				calories: 200,
+				fat: 5,
+				carbs: 30,
+				protein: 10
+			},
+			{
+				name: 'Toast',
+				calories: 400,
+				fat: 20,
+				carbs: 10,
+				protein: 30
+			},
+			{
+				name: 'Oatmeal',
+				calories: 500,
+				fat: 30,
+				carbs: 20,
+				protein: 15
+			}
+		],
+		meals: [
+			{
+				name: 'Bagel',
+				calories: 300,
+				fat: 10,
+				carbs: 20,
+				protein: 5
+			},
+			{
+				name: 'Eggs',
+				calories: 200,
+				fat: 5,
+				carbs: 30,
+				protein: 10
+			},
+			{
+				name: 'Toast',
+				calories: 400,
+				fat: 20,
+				carbs: 10,
+				protein: 30
+			},
+			{
+				name: 'Oatmeal',
+				calories: 500,
+				fat: 30,
+				carbs: 20,
+				protein: 15
+			}
+		],
+		dinners: [
+			{
+				name: 'Bagel',
+				calories: 300,
+				fat: 10,
+				carbs: 20,
+				protein: 5
+			},
+			{
+				name: 'Eggs',
+				calories: 200,
+				fat: 5,
+				carbs: 30,
+				protein: 10
+			},
+			{
+				name: 'Toast',
+				calories: 400,
+				fat: 20,
+				carbs: 10,
+				protein: 30
+			},
+			{
+				name: 'Oatmeal',
+				calories: 500,
+				fat: 30,
+				carbs: 20,
+				protein: 15
+			}
+		]
+	};
+</script>
+
+<h2>Breakfast</h2>
+
+<table class="table">
+  <thead>
+    <tr>
+      <th>name</th>
+      <th>calories</th>
+      <th>carbs</th>
+      <th>protein</th>
+      <th>fat</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td scope="row"></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+  </tbody>
+</table>
