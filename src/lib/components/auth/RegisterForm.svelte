a22f23cb08b12d545629c0ceb9d3cd8a514f832b
Sergio Junior Castro Egurrolar <v-secastro@expediagroup.com>
2022-07-04 16:44:36 -0700

    Added register form

---
+++ src/lib/components/auth/RegisterForm.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+
+	import { supabase } from '$lib/database';
+
+	let email: string;
+	let password: string;
+
+	async function handleSignIn() {
+		const { error } = await supabase.auth.signIn({ email, password });
+		if (error) alert(error.message);
+		goto('/');
+	}
+</script>
+
+<form on:submit|preventDefault={handleSignIn}>
+	<div class="mt-3">
+		<input class="form-control" type="email" placeholder="Email" bind:value={email} />
+	</div>
+	<div class="mt-3">
+		<input class="form-control" type="password" placeholder="Password" bind:value={password} />
+	</div>
+	<div class="mt-3">
+		<button class="btn btn-success" type="submit">Sign In</button>
+	</div>
+</form>
